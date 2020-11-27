9213c1e8e84de247c45f259328c5fecd2c630c06
g19980808 <3575912470@qq.com>
2020-11-27 13:59:21 +0800

    'kk'

--- 找房无忧/js/list.js
+++ 找房无忧/js/list.js
@@ -2,43 +2,47 @@ var middle=document.getElementsByClassName('middle')[0];
 middle.onclick=function(){
     window.open('./my.html')
 }
-//切换
+
+/* ------切换-------------------------------- */
+
 $('.category>li').on('click',function(){
     $(".category>li").find('.box1').css('display','none');
     $(this).find('.box1').css('display','block');
-
+    $(this).css('color','#ff8a00');
+    $(this).siblings().css('color','#000');
+    $('.wrap>.mantle').css('display','block');
+    $(this).find('.h-down').removeClass('h-down').addClass('y-up'); 
+    $(this).siblings().find('.y-up').removeClass('y-up').addClass('h-down');
 })
-// 1. 隐藏
-/* $(".category>li").find('.box1').on('touchstart',function(){
-    $(this).css('display','none');
-}) */
-// 2. 隐藏
- $(".price .box1").find('li').on('touchstart',function(){
-    $(this).parents('.box1').css('display','none');
-}) 
+/* ----------------------------------------- */
 
-// 3. 隐藏
+
+    $(".mantle").on('touchstart',function(){
+        $(this).siblings().find('.box1').css('display','none');
+        $(this).css('display','none');
+        $(this).siblings().find('li').css('color','#000');
+        $(this).siblings().find('.y-up').removeClass('y-up').addClass('h-down');
+    })
+
+
+ 
  $('.house li:last').on('touchstart',function(){
     $(this).parents('.box1').hide();
-
+    $(this).parents('.category').siblings('.mantle').css('display','none')
+    $(this).parents('li').css('color','#000');
+    $(this).parents('li').find('.y-up').removeClass('y-up').addClass('h-down');
+}) 
+ $('.confirm').on('touchstart',function(){
+    $(this).parents('.box1').hide();
+    $(this).parents('.category').siblings('.mantle').css('display','none')
+    $(this).parents('li').css('color','#000');
+    $(this).parents('li').find('.y-up').removeClass('y-up').addClass('h-down');
 }) 
 // 4. 隐藏
 /*  $('.more li').on('touchstart',function(){
     $(this).parents('.box1').hide();
 
 })  */
-
-
-
-//切换
-$('.category>li').on('click',function(){
-    $(".category>li").find('.box1').css('display','none');
-    $(this).find('.box1').css('display','block');
-    $(this).find('.h-down').addClass('h-down');
-    $(this).find('.h-down').removeClass('h-down').addClass('y-up');
-
-})
-
 
 
 $('.more').find('li').on('click',function(){
